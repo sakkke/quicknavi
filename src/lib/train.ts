5a8414c82f06ba3017f23f6cf0dd6c5f5738f159
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-08-16 06:42:21 +0900

    feat: add train.ts

---
+++ src/lib/train.ts
@@ -0,0 +1,16 @@
+import trainDefault from '../assets/Train Default.svg'
+import trainJT from '../assets/Train 東海道線.svg'
+import trainOH from '../assets/Train 小田原線.svg'
+
+export function getTrainPreview(name: string): string {
+  switch (name) {
+    case '東海道線':
+      return trainJT
+    case '小田原線':
+    case '江ノ島線':
+    case '多摩線':
+      return trainOH
+    default:
+      return trainDefault
+  }
+}
