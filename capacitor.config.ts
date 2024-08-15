97d6c5c211a78237622f41da4cb0a2e08c932e2c
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-08-16 08:38:34 +0900

    chore: npx cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.sakkke.quicknavi',
+  appName: 'quicknavi',
+  webDir: 'dist'
+};
+
+export default config;
