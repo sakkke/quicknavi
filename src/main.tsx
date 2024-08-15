c978bd8de4c3c9af8ba0285963557f767dfcf70e
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-08-15 17:03:48 +0900

    feat: add App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,10 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode />
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
