10c2cd7300f70e4c17bb191bb0ba7d8940b2dbe2
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-08-16 17:18:21 +0900

    fix: fix warnings

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,7 @@
 import { SupabaseClient, createClient as supabaseCreateClient } from '@supabase/supabase-js'
 
-export function createClient(): SupabaseClient {
-  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!
-  const supabaseKey = import.meta.env.VITE_SUPABASE_KEY!
-  const supabase = supabaseCreateClient(supabaseUrl, supabaseKey)
-  return supabase
-}
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY!
+const supabase = supabaseCreateClient(supabaseUrl, supabaseKey)
+
+export const createClient = (): SupabaseClient => supabase
